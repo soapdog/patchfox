ed5c998f67fde770053d732c91eab1debc8f4d41
andre alves garzia <andre@andregarzia.com>
2018-02-26 20:04:30 -0300

    feature(host app): implement tray app and permission system (naive)

--- host_app/server.js
+++ host_app/server.js
@@ -1,45 +1,64 @@
 #! /usr/bin/env node
 
-var http = require('http')
-var serve = require('ecstatic')
-var fs = require('fs')
-var path = require('path')
-var ssbKeys = require('ssb-keys')
-var minimist = require('minimist')
-var serverDiscovery = require('ssb-server-discovery')
-var eventEmitter = serverDiscovery.eventEmitter
+const http = require('http')
+const serve = require('ecstatic')
+const fs = require('fs')
+const path = require('path')
+const ssbKeys = require('ssb-keys')
+const minimist = require('minimist')
+const serverDiscovery = require('ssb-server-discovery')
+const eventEmitter = serverDiscovery.eventEmitter
 const notifier = require('node-notifier')
+const SysTray = require('systray').default
+let shouldGrantPerms = false
 
 eventEmitter.on('server-discovery-request', (origin) => {
   console.log("######### DISCOVERY REQUEST #############", origin)
+  let msg
+  let action
+
+  if (shouldGrantPerms) {
+    action = "granted"
+  } else {
+    action = "denied"
+  }
+
+  if (origin.startsWith("moz-extension://")) {
+    msg = `Firefox Add-on ${origin} ${action} access to sbot.`
+  } else {
+    msg = `Web Application ${origin} ${action} access to sbot.`
+  }
+
+  eventEmitter.emit('server-discovery-response', origin, shouldGrantPerms)
+
   notifier.notify({
     title: 'Secure Scuttlebutt',
-    message: `Application ${origin} wants to access sbot`, // String. Required if remove is not defined
-    icon: path.join(__dirname, "icon.png"), // String. Absolute path to Icon
-    wait: true, // Bool. Wait for User Action against Notification or times out
-    id: 0, // Number. ID to use for closing notification.
+    message: msg,
+    icon: path.join(__dirname, "icon.png"),
+    wait: true,
+    id: 0,
   })
 })
 
-var argv = process.argv.slice(2)
-var i = argv.indexOf('--')
-var conf = argv.slice(i + 1)
+let argv = process.argv.slice(2)
+let i = argv.indexOf('--')
+let conf = argv.slice(i + 1)
 argv = ~i ? argv.slice(0, i) : argv
 
-var config = require('ssb-config/inject')(process.env.ssb_appname, minimist(conf))
+const config = require('ssb-config/inject')(process.env.ssb_appname, minimist(conf))
 
-var keys = ssbKeys.loadOrCreateSync(path.join(config.path, 'secret'))
+const keys = ssbKeys.loadOrCreateSync(path.join(config.path, 'secret'))
 if (keys.curve === 'k256') {
   throw new Error('k256 curves are no longer supported,' +
     'please delete' + path.join(config.path, 'secret'))
 }
 
-var manifestFile = path.join(config.path, 'manifest.json')
+const manifestFile = path.join(config.path, 'manifest.json')
 
 // special server command:
 // import sbot and start the server
 
-var createSbot = require('scuttlebot')
+const createSbot = require('scuttlebot')
   // .use(require('scuttlebot/plugins/plugins'))
   .use(require('scuttlebot/plugins/master'))
   .use(require('scuttlebot/plugins/gossip'))
@@ -68,7 +87,76 @@ var createSbot = require('scuttlebot')
 // start server
 
 config.keys = keys
-var server = createSbot(config)
+const server = createSbot(config)
 
 // write RPC manifest to ~/.ssb/manifest.json
 fs.writeFileSync(manifestFile, JSON.stringify(server.getManifest(), null, 2))
+
+const icon = fs.readFileSync(path.join(__dirname, `icon.${process.platform === 'win32' ? 'ico' : 'png'}`))
+const tray = new SysTray({
+  menu: {
+    icon: icon.toString('base64'),
+    title: 'Secure Scuttlebutt',
+    tooltip: 'Secure Scuttlebutt tray app',
+    items: [
+      {
+        title: 'Grant connections',
+        tooltip: 'If enabled grant connection for applications',
+        checked: shouldGrantPerms,
+        enabled: true
+      },
+      {
+        title: 'Quit',
+        tooltip: 'Stop sbot and quit tray application',
+        checked: false,
+        enabled: true
+      }
+    ]
+  },
+  debug: false,
+  copyDir: true,
+})
+
+tray.onClick(action => {
+  switch (action.seq_id) {
+    case 0:
+      shouldGrantPerms = !shouldGrantPerms
+      tray.sendAction({
+        type: 'update-item',
+        item: {
+          ...action.item,
+          checked: shouldGrantPerms,
+        },
+        seq_id: action.seq_id,
+      })
+
+      let permMsg = shouldGrantPerms ? "grant" : "deny"
+
+      notifier.notify({
+        title: 'Secure Scuttlebutt',
+        message: `Secure Scuttlebutt will ${permMsg} connections to sbot`,
+        icon: path.join(__dirname, "icon.png"),
+        wait: true,
+        id: 0,
+      })
+
+      break
+    case 1:
+      console.log("### EXITING IN TWO SECONDS ###")
+
+      notifier.notify({
+        title: 'Secure Scuttlebutt',
+        message: `Secure Scuttlebutt will exit in two seconds...`,
+        icon: path.join(__dirname, "icon.png"),
+        wait: true,
+        id: 0,
+      })
+
+      tray.kill()
+  }
+})
+
+tray.onExit((code, signal) => {
+  setTimeout(() =>
+    process.exit(0), 2000)
+})
