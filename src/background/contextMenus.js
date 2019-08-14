824f999c9dcd6788172be3ffa39df673de6ca93b
Andre Alves Garzia <andre@andregarzia.com>
2019-08-14 02:02:06 +0100

    context menu: copy a link to this page

--- src/background/contextMenus.js
+++ src/background/contextMenus.js
@@ -26,14 +26,25 @@ function copyToClipboard(text, html) {
 function initializeContextMenus() {
     browser.contextMenus.create({
         id: "text-selection-to-clipboard-as-quotation",
-        title: "Copy Selected Text as Quotation",
+        title: "Copy selected text as quotation",
         contexts: ["selection"]
     }, onCreated);
 
+    browser.contextMenus.create({
+        id: "page-action-to-clipboard-as-link",
+        title: "Copy link to this page",
+        contexts: ["all","page_action"]
+    }, onCreated);
+
     browser.contextMenus.onClicked.addListener(function (info, tab) {
+        let template
         switch (info.menuItemId) {
             case "text-selection-to-clipboard-as-quotation":
-                let template = `> ${info.selectionText}\n&mdash; _Source: [${tab.title}](${info.pageUrl})_`
+                template = `> ${info.selectionText}\n&mdash; _Source: [${tab.title}](${info.pageUrl})_`
+                copyToClipboard(template, template)
+                break;
+            case "page-action-to-clipboard-as-link":
+                template = `[${tab.title}](${tab.url})`
                 copyToClipboard(template, template)
                 break;
         }
