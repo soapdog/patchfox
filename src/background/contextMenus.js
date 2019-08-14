b0b1d7739057aef6669109fc45a51129adec6339
Andre Alves Garzia <andre@andregarzia.com>
2019-08-14 02:16:42 +0100

    context menu: rewording

--- src/background/contextMenus.js
+++ src/background/contextMenus.js
@@ -32,12 +32,19 @@ function initializeContextMenus() {
 
     browser.contextMenus.create({
         id: "page-action-to-clipboard-as-link",
-        title: "Copy link to this page",
-        contexts: ["all","page_action"]
+        title: "Copy link to the current page",
+        contexts: ["all", "page_action"]
+    }, onCreated);
+
+    browser.contextMenus.create({
+        id: "link-to-clipboard-as-link",
+        title: "Copy link",
+        contexts: ["link"]
     }, onCreated);
 
     browser.contextMenus.onClicked.addListener(function (info, tab) {
         let template
+        console.dir("info", info)
         switch (info.menuItemId) {
             case "text-selection-to-clipboard-as-quotation":
                 template = `> ${info.selectionText}\n&mdash; _Source: [${tab.title}](${info.pageUrl})_`
@@ -45,6 +52,10 @@ function initializeContextMenus() {
                 break;
             case "page-action-to-clipboard-as-link":
                 template = `[${tab.title}](${tab.url})`
+                copyToClipboard(template, template)
+                break;
+            case "link-to-clipboard-as-link":
+                template = `[${info.linkText}](${info.linkUrl})`
                 copyToClipboard(template, template)
                 break;
         }
