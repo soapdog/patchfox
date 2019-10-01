6b601d3abacf21ff6b78a5861053eb277eeb2ce7
Andre Alves Garzia <andre@andregarzia.com>
2019-10-01 14:03:47 +0100

    not working

--- src/packages/contacts/Following.svelte
+++ src/packages/contacts/Following.svelte
@@ -1,44 +1,44 @@
 <script>
   const QueryRepeater = require("../../core/components/parts/QueryRepeater.svelte");
   const AvatarChip = require("../../core/components/parts/AvatarChip.svelte");
+  const pull = require("pull-stream");
+  const _ = require("lodash");
+
+  const sbot = ssb.sbot;
 
   export let feed;
 
   let contacts = [];
 
-  let query = [
-    {
-      $filter: {
-        value: {
-          author: feed,
-          content: {
-            type: "contact"
-          }
-        }
-      }
-    }
-  ];
-
   pull(
-      sbot.query.read({
-          query
-      }),
-      pull.drain(msg => {
-          if (!msg.sync) {
-              let feed = msg.value.content.contact
-              let following = msg.value.content.following
-              let blocking = msg.value.content.blocking
-              contacts[key] = {
-                  feed,
-                  // todo: arrgghhh!! use follow = true and not blocking
-
-              }
-          }
-      })
-  )
+    sbot.createUserStream({id: feed, reverse: true}),
+    pull.filter(msg => _.get(msg,"msg.value.content.type", false) === "contact"),
+    // pull.filter(msg => typeof contacts[msg] !== "undefined"),
+    pull.drain(msg => {
+      console.log(msg)
+      if (!msg.sync) {
+        let contact = msg.value.content.contact;
+        let following = msg.value.content.following;
+        let blocking = msg.value.content.blocking;
+        contacts[contact] = {
+          contact,
+          following,
+          blocking
+          // todo: arrgghhh!! use follow = true and not blocking
+        };
+        console.log("adding", contact)
+      } else {
+        console.log("acabou", contacts)
+        contacts = contacts
+      }
+    })
+  );
 </script>
 
-
 {#each contacts as contact}
-  <AvatarChip feed={msg.value.content.contact} />
+  {#if contact.following}
+    <AvatarChip class="" feed={contact.feed} />
+  {/if}
+{:else}
+  <div class="loading" />
 {/each}
