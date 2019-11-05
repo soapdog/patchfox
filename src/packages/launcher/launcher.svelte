bdd1f13c7bae78068eb844112d5a2f3ed958c1cf
Andre Alves Garzia <andre@andregarzia.com>
2019-11-05 14:49:40 +0000

    [[ launcher ]] intial list of packages work

--- src/packages/launcher/launcher.svelte
+++ src/packages/launcher/launcher.svelte
@@ -3,6 +3,10 @@
 
   export let active = false;
 
+  let packageKeys = Object.keys(patchfox.packages).filter(pkg => {
+    return patchfox.packages[pkg].hasOwnProperty("view");
+  });
+
   let removeHotKey = keymage(
     "ctrl-m",
     () => {
@@ -12,6 +16,21 @@
     { preventDefault: true }
   );
 
+  const iconForPackage = pkg => {
+    let icon = patchfox.packages[pkg].icon || "/images/package.svg";
+    return icon;
+  };
+
+  const titleForPackage = pkg => {
+    let name = patchfox.packages[pkg].title || patchfox.packages[pkg].name;
+    return name;
+  };
+
+  const urlForPackage = pkg => {
+    let url = `?pkg=${pkg}`;
+    return url;
+  };
+
   onDestroy(() => {
     keymage.popScope("launcher");
     removeHotKey();
@@ -19,7 +38,21 @@
 </script>
 
 <style>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+    grid-auto-rows: auto;
+    grid-gap: 1rem;
+  }
+  .package {
+    border-radius: 4px;
+    padding: 0.5rem;
+    text-align: center;
+  }
 
+  .package-title {
+    text-transform: capitalize;
+  }
 </style>
 
 <div class:active class="modal modal-lg">
@@ -27,11 +60,25 @@
   <div class="modal-container">
     <div class="modal-header">
       <a href="#close" class="btn btn-clear float-right" aria-label="Close" />
-      <div class="modal-title h5">Modal title</div>
+      <div class="modal-title h5">Packages</div>
     </div>
     <div class="modal-body">
-      <div class="content">
-        <!-- content here -->
+      <div class="grid">
+        {#each packageKeys as pkg}
+          <div class="package">
+            <div class="package-icon">
+              <img
+                class="centered"
+                src={iconForPackage(pkg)}
+                alt={titleForPackage(pkg)} />
+            </div>
+            <div class="package-content">
+              <div class="package-title" href={urlForPackage(pkg)}>
+                {titleForPackage(pkg)}
+              </div>
+            </div>
+          </div>
+        {/each}
       </div>
     </div>
     <div class="modal-footer">...</div>
