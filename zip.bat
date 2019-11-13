5ddca14f4160b0f718a6efc97c2ae997850d12a6
Andre Alves Garzia <andre@andregarzia.com>
2019-11-13 19:35:23 +0000

    new zip.bat

--- zip.bat
+++ zip.bat
@@ -1,6 +1,9 @@
 @echo off
+echo "building web extension..."
 cd .\dist
 call web-ext build --overwrite-dest
 move .\web-ext-artifacts ..\
 cd ..
-git archive -o source.zip HEAD
+echo  "compressing source.."
+call git archive -o source.zip HEAD
+echo "done"
