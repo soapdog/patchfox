3af84580c0dd255b4f586d841d052c41edf97cca
Andre Alves Garzia <andre@andregarzia.com>
2019-11-13 19:39:47 +0000

    mini fixes

--- zip.bat
+++ zip.bat
@@ -1,9 +1,11 @@
 @echo off
-echo "building web extension..."
+echo rebuilding web extension...
+call npm run clean-build
+echo building web extension...
 cd .\dist
 call web-ext build --overwrite-dest
 move .\web-ext-artifacts ..\
 cd ..
-echo  "compressing source.."
+echo compressing source..
 call git archive -o source.zip HEAD
-echo "done"
+echo done
