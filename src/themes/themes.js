b5154f2c7d979e5f0a4021177a2a9a1db5ecce69
Andre Alves Garzia <andre@andregarzia.com>
2021-02-20 15:39:47 +0000

    [[ css ]] fix  for broken CSS import

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -1,4 +1,4 @@
-require("../../node_modules/spectre.css/dist/spectre-icons.min.css");
+require("./default/spectre-icons.min.css");
 require("./default/default.scss");
 
-module.exports = {}
+module.exports = {};
