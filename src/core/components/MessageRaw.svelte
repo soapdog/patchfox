3dd5cbb3c95dbfcd5afeb8a2590d708e2866a773
Andre Alves Garzia <andre@andregarzia.com>
2021-05-18 16:33:50 +0100

    [[ message raw ]] new access to documentation
    Allows the user viewing a _raw message_ to check the docs for the message type, the package that is handling it, and even the package source code.

--- src/core/components/MessageRaw.svelte
+++ src/core/components/MessageRaw.svelte
@@ -9,14 +9,10 @@
     type = msg.value.content.type;
   }
 
-  let raw = JSON.stringify(msg, null, 4)
-</script>
+  let raw = JSON.stringify(msg, null, 4);
 
-<style>
-.container {
-    overflow: scroll;
-}
-</style>
+  let pkg = patchfox.packageForType(msg);
+</script>
 
 <div class="columns">
   <div class="column col-9">
@@ -37,5 +33,33 @@
       </a>
       .
     </p>
+    {#if pkg}
+      <p>
+        This message is being handled by the <b>{pkg.name} Package</b>.
+        <a target="_blank" href="/docs/index.html#/packages/{pkg.name}/">
+          Click here to check more about it.
+        </a>
+      </p>
+      <p>Do you want to see the source-code for this package?</p>
+
+      <ul>
+        <li>
+          <a target="_blank" href="https://github.com/soapdog/patchfox/blob/master/src/packages/{pkg.name}/">
+            See source for <b>{pkg.name} package</b> at GitHub.
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://git.sr.ht/~soapdog/patchfox/tree/master/item/src/packages/{pkg.name}/">
+            See source for <b>{pkg.name} package</b> at SourceHut.
+          </a>
+        </li>
+      </ul>
+    {/if}
   </div>
 </div>
+
+<style>
+  .container {
+    overflow: scroll;
+  }
+</style>
