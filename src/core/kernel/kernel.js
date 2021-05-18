9249c8a88797408f58dd3fd4b93b5ffcea56b40c
Andre Alves Garzia <andre@andregarzia.com>
2021-05-18 16:32:57 +0100

    [[ kernel ]] new `patchfox.packageForType()`
    Returns the package that is able to handle `msg`.

--- src/core/kernel/kernel.js
+++ src/core/kernel/kernel.js
@@ -62,12 +62,63 @@ function url(pkg, view, data) {
   return `/index.html?${qs}`
 }
 
+function packageForType(msg) {
+  let messageTypes = [];
+
+  let packagesForMessageTypes = _.filter(
+    patchfox.packages,
+    p => p.messageTypes
+  );
+
+  let type;
+
+  const makeGenericValidatorForType = typeToBuildFor => {
+    return msg => {
+      let type;
+      if (typeof msg.value.content === "string") {
+        type = "private";
+      } else {
+        type = msg.value.content.type;
+      }
+      return type === typeToBuildFor;
+    };
+  };
+
+  packagesForMessageTypes.forEach(p => {
+    p.messageTypes.forEach(mt => {
+      let type = mt.type;
+      let view = mt.card;
+      let short = mt.short || false;
+      let validator = mt.validator || makeGenericValidatorForType(type);
+      messageTypes.push({ type, validator, view, short, pkg: p });
+    });
+  });
+
+  let selectedPackage = false;
+
+  if (typeof msg.value.content === "string") {
+    type = "private";
+  } else {
+    type = msg.value.content.type;
+  }
+
+  for (let p of messageTypes) {
+    if (p.validator(msg)) {
+      selectedPackage = p.pkg;
+      break;
+    }
+  }
+
+  return selectedPackage
+}
+
 module.exports = {
   // package related
   package,
   packages,
   systemPackages,
   appPackages,
+  packageForType,
   go,
   url,
   reload,
