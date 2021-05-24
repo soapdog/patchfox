e2e7efa9bd6de5e78dc6034b26eb3baaaefb74dd
Andre Alves Garzia <andre@andregarzia.com>
2021-05-24 19:21:13 +0100

    [[ webextension ]] adding browser polyfill.
    This fixes the lack of `browser.*` API namespace on chromium-based browsers. Patchfox had the polyfill vendored and loaded in some pages, but that was not enough to add it to all the places it needed to be added.
    
    I've switched from the vendored copy to a NPM module for the polyfill. Added a task to copy it around, and added it  to many places where it was needed.

--- src/core/background/caches.js
+++ src/core/background/caches.js
@@ -2,65 +2,66 @@ const paramap = require("pull-paramap");
 const pull = require("pull-stream");
 const _ = require("lodash");
 const kernel = require("../kernel/kernel.js")
+const browser = require("webextension-polyfill");
 
 
 let friends = []
 
 const connectSSB = async () => {
-    try {
-        let savedData = await kernel.loadConfiguration()
-        // window.ssb.* comes from browserified ssb.js
-        // that exists only in the dist folder.
-        let server = await ssb.connect(kernel.savedKeys(), savedData.remote)
-        window.ssb.feed = server.id
-        window.ssb.sbot = server
-        ssb.setGetPrefFunction(kernel.getPref)
-        ssb.setIsMessageHiddenFunction(isMessageHidden)
-        await ssb.loadCaches()
-        return server.id
-    } catch (n) {
-        console.error(`can't connect to sbot`, n)
-    }
+  try {
+    let savedData = await kernel.loadConfiguration()
+    // window.ssb.* comes from browserified ssb.js
+    // that exists only in the dist folder.
+    let server = await ssb.connect(kernel.savedKeys(), savedData.remote)
+    window.ssb.feed = server.id
+    window.ssb.sbot = server
+    ssb.setGetPrefFunction(kernel.getPref)
+    ssb.setIsMessageHiddenFunction(isMessageHidden)
+    await ssb.loadCaches()
+    return server.id
+  } catch (n) {
+    console.error(`can't connect to sbot`, n)
+  }
 }
 
 const computeFriendsCache = () => {
-    const sbot = ssb.sbot
+  const sbot = ssb.sbot
 
-    pull(
-        sbot.links({
-            source: feed,
-            rel: "contact",
-            values: true,
-            reverse: true
-        }),
-        pull.map(function (msg) {
-            return msg && msg.value && msg.value.content;
-        }),
-        pull.filter(function (content) {
-            return content && content.type === "contact";
-        }),
-        pull.unique("contact"),
-        pull.filter(function (content) {
-            return content.following === true;
-        }),
-        paramap((content, cb) => {
-            ssb.following(feed, content.contact)
-                .then(data => {
-                    content.friend = data
-                    cb(null, content)
-                })
-
-        }),
-        pull.filter(content => content.friend),
-        pull.map("contact"),
-        pull.drain((id) => {
-            friends.push(id)
+  pull(
+    sbot.links({
+      source: feed,
+      rel: "contact",
+      values: true,
+      reverse: true
+    }),
+    pull.map(function (msg) {
+      return msg && msg.value && msg.value.content;
+    }),
+    pull.filter(function (content) {
+      return content && content.type === "contact";
+    }),
+    pull.unique("contact"),
+    pull.filter(function (content) {
+      return content.following === true;
+    }),
+    paramap((content, cb) => {
+      ssb.following(feed, content.contact)
+        .then(data => {
+          content.friend = data
+          cb(null, content)
         })
-    );
+
+    }),
+    pull.filter(content => content.friend),
+    pull.map("contact"),
+    pull.drain((id) => {
+      friends.push(id)
+    })
+  );
 }
 
 module.exports = {
-    friends,
-    computeFriendsCache,
-    connectSSB
+  friends,
+  computeFriendsCache,
+  connectSSB
 }
