6cf2c30ef4b84f5ef6dde98e0b30ccad5027303b
Andre Alves Garzia <andre@andregarzia.com>
2021-07-14 13:58:22 +0100

    [[ core ]] output version to console on startup.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const packages = require("./packages/packages.js");
 const themes = require("./themes/themes.js");
 const WM = require("./wm.svelte");
 
+let version = browser.runtime.getManifest().version
+console.info(`Patchfox Version ${version}`)
+
 core.start().then(() => {
   const wm = new WM({
     target: document.body
