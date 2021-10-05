0e856ea7181079be7846aec9c26ebddf4c101f29
Andre Alves Garzia <andre@andregarzia.com>
2021-10-05 15:25:33 +0100

    extra features for avatarchip

--- src/core/components/AvatarChip.svelte
+++ src/core/components/AvatarChip.svelte
@@ -29,8 +29,8 @@
 
 
 {#if image}
-<div class="{flexClass} items-center p-1 gap-1 justify-center">
-  <figure class="avatar cursor-pointer" on:click="{avatarClick}">
+<div class="{flexClass} items-center p-1 gap-1 justify-center cursor-pointer" on:click="{avatarClick}">
+  <figure class="avatar">
     <div class="mb-1 w-4 h-4 mask mask-squircle">
       <img src="{image}" alt="{name}" />
     </div>
@@ -38,8 +38,8 @@
   <span class="flex-1 inline-block align-middle">{name}</span>
 </div>
 {:else}
-<div class="{flexClass} items-center p-1 gap-1 justify-center">
-  <figure class="avatar cursor-pointer" data-initial="{name.slice(1,3)}" on:click="{avatarClick}">
+<div class="{flexClass} items-center p-1 gap-1 justify-center cursor-pointer" on:click="{avatarClick}">
+  <figure class="avatar" data-initial="{name.slice(1,3)}">
     <div class="mb-1 bg-neutral-focus text-neutral-content w-4 h-4 mask mask-squircle">
       <span class="text-xl">{name.slice(1,3)}</span>
     </div>
