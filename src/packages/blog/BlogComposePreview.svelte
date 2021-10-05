2c9c8173070819815812fcf1ed9f801e50b3abc2
Andre Alves Garzia <andre@andregarzia.com>
2021-10-05 11:25:22 +0100

    blog package updated. Missing the apps.

--- src/packages/blog/BlogComposePreview.svelte
+++ src/packages/blog/BlogComposePreview.svelte
@@ -1,10 +1,10 @@
 <script>
-export let channel
-export let title
-export let contentWarning
-export let content
-export let summary
-export let thumbnail
+  export let channel
+  export let title
+  export let contentWarning
+  export let content
+  export let summary
+  export let thumbnail
 </script>
 
 <style>
@@ -13,44 +13,32 @@ export let thumbnail
   }
 </style>
 
-<h2>Post preview</h2>
-{#if channel || contentWarning.length > 0 || title || thumbnail}
-  <blockquote>
-    {#if channel}
-      <p>
-        <b>Channel:</b>
-        {channel.startsWith('#') ? channel.slice(1) : channel}
-      </p>
-    {/if}
-    {#if contentWarning.length > 0}
-      <p>
-        <b>Content Warning:</b>
-        {contentWarning}
-      </p>
-    {/if}
-    {#if title}
-      <p>
-        <b>title:</b>
-        {title}
-      </p>
-    {/if}
-    {#if summary}
-      <p>
-        <b>Summary:</b>
-        {@html ssb.markdown(summary)}
-      </p>
-    {/if}
-    {#if thumbnail}
-      <p>
-        <b>Thumbnail:</b>
-        <img
-          class="thumbnail-preview"
-          src="{patchfox.bloburl(thumbnail)}"
-          alt="post thumbnail" />
-      </p>
-    {/if}
-  </blockquote>
-{/if}
-{@html ssb.markdown(content)}
-
-
+<article class="prose">
+  <h2>Blog post preview</h2>
+  {#if channel || contentWarning.length > 0 || title || thumbnail} {#if channel}
+  <p>
+    <b>Channel:</b>
+    {channel.startsWith("#") ? channel.slice(1) : channel}
+  </p>
+  {/if} {#if contentWarning.length > 0}
+  <p>
+    <b>Content Warning:</b>
+    {contentWarning}
+  </p>
+  {/if} {#if title}
+  <p>
+    <b>title:</b>
+    {title}
+  </p>
+  {/if} {#if summary}
+  <p>
+    <b>Summary:</b>
+    {@html ssb.markdown(summary)}
+  </p>
+  {/if} {#if thumbnail}
+  <p>
+    <b>Thumbnail:</b>
+    <img class="thumbnail-preview" src="{patchfox.bloburl(thumbnail)}" alt="post thumbnail" />
+  </p>
+  {/if} {/if} {@html ssb.markdown(content)}
+</article>
