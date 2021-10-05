1a86191e32f4471e4c11bcaa7c651336be19667a
Andre Alves Garzia <andre@andregarzia.com>
2021-10-05 13:38:30 +0100

    gatherings package updated

--- src/packages/calendar/GatheringCard.svelte
+++ src/packages/calendar/GatheringCard.svelte
@@ -1,53 +1,53 @@
 <script>
-  const Card = require("../../core/components/ui/Card.svelte");
-  const Scuttle = require("scuttle-gathering");
-  const gathering = Scuttle(ssb.sbot);
+  const Card = require("../../core/components/ui/Card.svelte")
+  const Scuttle = require("scuttle-gathering")
+  const gathering = Scuttle(ssb.sbot)
 
-  export let msg;
+  export let msg
 
-  let msgid = msg.key;
-  let person = msg.value.author;
-  let event = false;
-  let showRaw = false;
-  let attending;
-  let notAttending;
+  let msgid = msg.key
+  let person = msg.value.author
+  let event = false
+  let showRaw = false
+  let attending
+  let notAttending
 
-  ssb.avatar(msg.value.author).then(data => (person = data.name));
+  ssb.avatar(msg.value.author).then(data => (person = data.name))
 
   gathering.get(msgid, (err, data) => {
-    console.log(data);
+    console.log(data)
     if (!err) {
-      event = data;
-      attending = event.isAttendee;
-      notAttending = data.notAttendees.includes(ssb.feed);
+      event = data
+      attending = event.isAttendee
+      notAttending = data.notAttendees.includes(ssb.feed)
     }
-  });
+  })
 
   const dateToNiceDate = epoch => {
-    let date = new Date(epoch).toLocaleDateString();
-    let time = new Date(epoch).toLocaleTimeString();
-    return `${date} ${time}`;
-  };
+    let date = new Date(epoch).toLocaleDateString()
+    let time = new Date(epoch).toLocaleTimeString()
+    return `${date} ${time}`
+  }
 
   const attend = () => {
     gathering.attending(msgid, true, (err, data) => {
-      console.log(err);
-      console.log(data);
+      console.log(err)
+      console.log(data)
       if (!err) {
-        attending = true;
-        notAttending = false;
+        attending = true
+        notAttending = false
       }
-    });
-  };
+    })
+  }
 
   const notAttend = () => {
     gathering.attending(msgid, false, (err, data) => {
       if (!err) {
-        notAttending = true;
-        attending = false;
+        notAttending = true
+        attending = false
       }
-    });
-  };
+    })
+  }
 </script>
 
 <style>
@@ -58,46 +58,42 @@
 
 <Card {msg} {showRaw}>
   {#if event}
-    <h1 class="title">{event.title}</h1>
+    <h1 class="uppercase font-medium">{event.title}</h1>
     {#if event.startDateTime}
-      <h2 class="subtitle">{dateToNiceDate(event.startDateTime.epoch)}</h2>
+      <h2 class="uppercase">{dateToNiceDate(event.startDateTime.epoch)}</h2>
     {/if}
     {#if event.image}
       <img
         class="gathering-image"
-        src={patchfox.httpUrl('/blobs/get/' + encodeURIComponent(event.image.link))}
+        src={patchfox.httpUrl("/blobs/get/" + encodeURIComponent(event.image.link))}
         alt={event.image.name} />
     {/if}
+    <div class="prose">
     {@html ssb.markdown(event.description)}
+    </div>
   {/if}
-  <div class="card-footer" slot="card-footer">
-    <div class="columns col-gapless">
-      <div class="column col-6">
-        <div class="btn-group btn-group-block">
+  <div class="card-actions" slot="card-actions">
+  
+        <div class="btn-group">
           <button
-            class="btn"
+            class="btn btn-outline"
             on:click={notAttend}
-            class:btn-primary={notAttending === true}>
+            class:btn-active={notAttending === true}>
             Not Attending
           </button>
           <button
-            class="btn"
+            class="btn btn-outline"
             on:click={attend}
-            class:btn-primary={attending === true}>
+            class:btn-active={attending === true}>
             Attending
           </button>
-        </div>
-      </div>
-
-      <div class="column col-6 text-right">
+    </div>
         <button
           class="btn"
           on:click={() => {
-            patchfox.go('calendar', 'gathering', { msgid });
+            patchfox.go("calendar", "gathering", { msgid })
           }}>
           View details
         </button>
-      </div>
-    </div>
   </div>
 </Card>
