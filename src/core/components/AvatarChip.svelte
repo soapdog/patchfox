4cde424728d9fda4e275c696aef9fc9ae028659d
Andre Alves Garzia <andre@andregarzia.com>
2021-10-05 14:21:58 +0100

    extra options for AvatarChip

--- src/core/components/AvatarChip.svelte
+++ src/core/components/AvatarChip.svelte
@@ -3,6 +3,12 @@
 
   export let feed
 
+  export let inline = false
+  export let arrow = false
+  export let glyph = false
+
+  let flexClass = inline ? "inline-flex" : "flex" 
+
   let image = false
   let name = feed
 
@@ -18,11 +24,12 @@
   const avatarClick = () => {
     dispatch("avatarClick", { feed, name })
   }
+
 </script>
 
 
 {#if image}
-<div class="flex items-center p-1 gap-1 justify-center">
+<div class="{flexClass} items-center p-1 gap-1 justify-center">
   <figure class="avatar cursor-pointer" on:click="{avatarClick}">
     <div class="mb-1 w-4 h-4 mask mask-squircle">
       <img src="{image}" alt="{name}" />
@@ -31,7 +38,7 @@
   <span class="flex-1 inline-block align-middle">{name}</span>
 </div>
 {:else}
-<div class="flex items-center p-1 gap-1 justify-center">
+<div class="{flexClass} items-center p-1 gap-1 justify-center">
   <figure class="avatar cursor-pointer" data-initial="{name.slice(1,3)}" on:click="{avatarClick}">
     <div class="mb-1 bg-neutral-focus text-neutral-content w-4 h-4 mask mask-squircle">
       <span class="text-xl">{name.slice(1,3)}</span>
@@ -39,4 +46,11 @@
   </figure>
   <span class="flex-1 inline-block align-middle">{name}</span>
 </div>
+{/if}
+{#if arrow}
+    <span class="mr-2">&rarr;</span>
+{/if}
+
+{#if glyph}
+    <span class="mr-2">{ssb.markdown(glyph,true)}</span>
 {/if}
