384421232d4f5d2b538ad0550242a6634d450e75
Andre Alves Garzia <andre@andregarzia.com>
2021-10-05 18:04:44 +0100

    pub pkg

--- src/packages/pub/Pub.svelte
+++ src/packages/pub/Pub.svelte
@@ -1,22 +1,26 @@
 <script>
-  export let msg;
+  const AvatarChip = require("../../core/components/AvatarChip.svelte")
 
-  let encodedid = encodeURIComponent(msg.value.content.address.key);
-  let person = msg.value.author;
+  export let msg
+
+  let encodedid = encodeURIComponent(msg.value.content.address.key)
+  let person = msg.value.author
   let host = msg.value.content.address.host
   let port = msg.value.content.address.port
 
-  ssb.avatar(msg.value.author).then(data => (person = data.name));
+  ssb.avatar(msg.value.author).then(data => (person = data.name))
 
   
   const goProfile = ev => {
-    ev.stopPropagation();
-    ev.preventDefault();
-    patchfox.go("contacts","profile", { feed: msg.value.content.address.key });
-  };
+    ev.stopPropagation()
+    ev.preventDefault()
+    patchfox.go("contacts","profile", { feed: msg.value.content.address.key })
+  }
 </script>
 
-<div class="card-body">
-   {person} announced pub
+<p class="m-2">
+  <AvatarChip inline={true} arrow={true} feed={msg.value.author} />
+  
+   announced pub
   <a href="?pkg=contacts&view=profile&feed={encodedid}" on:click={goProfile}>{host}:{port}</a>
-</div>
+</p>
