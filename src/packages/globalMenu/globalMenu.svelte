b90bf559a5dc3e440d7191bf4a2f34d8e9343cf4
Andre Alves Garzia <andre@andregarzia.com>
2022-04-01 14:03:17 +0100

    [[globalMenu]] better sidebar icon

--- src/packages/globalMenu/globalMenu.svelte
+++ src/packages/globalMenu/globalMenu.svelte
@@ -49,7 +49,7 @@
       {#if browserSidebarSupport}
       <div class="flex-none">
         <button class="btn btn-square btn-ghost" on:click={openSidebar}>
-            <i class="fas fa-arrow-left fa-lg" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sidebar"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="3" x2="9" y2="21"></line></svg>
         </button>
       </div>
       {/if}
